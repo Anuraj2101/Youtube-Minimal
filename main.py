add6000a22b17efd251d537943b35458594911f1
Anuraj Sanghu <anuraj.sanghu@ontariotechu.net>
2024-10-14 05:48:26 -0400

    Cleaning up code: Adding Search, Resp Parse and Resp Clean up

--- main.py
+++ main.py
@@ -10,10 +10,31 @@ from pyvidplayer2 import Video
 
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
-def main():
+def search():
+    query = str(input("Search: "))
+    return query
+
+def parse_results(resp):
+    cleaned_resp = []
+    for item in resp:    
+        if item["id"]["kind"] != "youtube#video":
+            continue
+        for key in item:
+            vid_id = item["id"]["videoId"]
+            vid_name = item["snippet"]["title"]
+            cleaned_resp.append(clean_results(vid_name, vid_id))
+            # print("Key:", key, "Value:", item[key])
+    return cleaned_resp
+
+def clean_results(video_name, video_id):
+    
+    return {video_name:f"https://youtube.com/watch?v={video_id}"}
+
+
+def main(search_string):
     # Disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "0"
 
     api_service_name = "youtube"
     api_version = "v3"
@@ -27,15 +48,18 @@ def main():
     request = youtube.search().list(
         part="snippet",
         maxResults=25,
-        q = "Marc Brunet"
+        q = search_string
     )
+
     response = request.execute()["items"]
-    for item in response:
-        for key in item: 
-            print(key, item[key])
+    search_results = parse_results(response)
+    
+    print(search_results)
+
     with open("Response.txt", "wb") as f:
         pickle.dump(str(response), f)
 
-    Video("https://www.youtube.com/watch?v=-EFmkxC8eBo", youtube=True).preview()
+    #Video("https://www.youtube.com/watch?v=-EFmkxC8eBo", youtube=True).preview()
 if __name__ == "__main__":
-    main()
+    query = search()
+    main(query)
