61d25d0a4e627fb433973c80f3e19a29a78c13b3
Anuraj Sanghu <anuraj.sanghu@ontariotechu.net>
2024-10-15 11:17:44 -0400

    Added menu and user selection feature

--- main.py
+++ main.py
@@ -19,17 +19,38 @@ def parse_results(resp):
     for item in resp:    
         if item["id"]["kind"] != "youtube#video":
             continue
-        for key in item:
-            vid_id = item["id"]["videoId"]
-            vid_name = item["snippet"]["title"]
-            cleaned_resp.append(clean_results(vid_name, vid_id))
-            # print("Key:", key, "Value:", item[key])
+        vid_id = item["id"]["videoId"]
+        vid_name = item["snippet"]["title"]
+        cleaned_resp.append(clean_results(str(vid_name).replace("&quot;", "\""), vid_id))
+
     return cleaned_resp
 
 def clean_results(video_name, video_id):
     
     return {video_name:f"https://youtube.com/watch?v={video_id}"}
 
+def menu(results):
+    num = 0
+    for result in results:
+        num +=1
+        print(f"{num}: {next(iter(result))} Link:{result[next(iter(result))]}")
+    
+    while True:
+        try:
+            selection = int(input("Which Search Would You Like to Access? (0 to Exit): "))
+        except Exception as e:
+            print(e)
+            continue
+
+        if selection < 0 or selection > len(results):
+            continue
+        else:
+            break 
+
+    if selection == 0:
+        return None
+    else:  
+        return (results[selection - 1][next(iter(results[selection - 1]))])
 
 def main(search_string):
     # Disable OAuthlib's HTTPS verification when running locally.
@@ -54,12 +75,13 @@ def main(search_string):
     response = request.execute()["items"]
     search_results = parse_results(response)
     
-    print(search_results)
+    link=menu(search_results)
+    print(link)
 
     with open("Response.txt", "wb") as f:
         pickle.dump(str(response), f)
 
-    #Video("https://www.youtube.com/watch?v=-EFmkxC8eBo", youtube=True).preview()
+    #Video(link, youtube=True).preview()
 if __name__ == "__main__":
     query = search()
     main(query)
